7ad042c31b89bbb7f93aca039819c8667b2d5246
ThietVo <vonhuthiet@gmail.com>
2022-06-14 09:08:06 +0700

    update server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000;
 
 server.use(middlewares)
-server.use((req, res, next) => {
- if (isAuthorized(req)) { // add your authorization logic here
-   next() // continue to JSON Server router
- } else {
-   res.sendStatus(401)
- }
-})
+// server.use((req, res, next) => {
+//  if (isAuthorized(req)) { // add your authorization logic here
+//    next() // continue to JSON Server router
+//  } else {
+//    res.sendStatus(401)
+//  }
+// })
 server.use(router)
 server.listen(port, () => {
   console.log('JSON Server is running')
